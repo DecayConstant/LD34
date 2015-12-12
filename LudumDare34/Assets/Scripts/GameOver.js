cb06dbe635c98c369e90828d82e7104243cc1962
Paul Stevenson <paul.mike.stevenson@gmail.com>
2015-12-12 13:38:11 -0600

    Added basic game timer and game over functionality

---
+++ LudumDare34/Assets/Scripts/GameOver.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+var gameover : boolean = false;
+public var gameover_panel : GameObject;
+
+function GameOver() {
+	gameover = true;
+	gameover_panel.active = true;
+}
+
+function Update() {
+	if(gameover && (Input.GetButtonDown("ButtonA") || Input.GetButtonDown("ButtonB"))) {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
---
+++ LudumDare34/Assets/Scripts/Timer.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+var gameover_script : GameOver;
+
+private var timer : float = 10;
+public var timer_string : UI.Text;
+
+function Update () {
+	
+	if(timer <= 0) {
+		gameover_script.GameOver();
+	}
+	else {
+		timer -= Time.deltaTime;
+		timer_string.text = timer.ToString("#.0") + "s";
+	}
+}
