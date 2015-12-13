6810fa545ad451814d2cdcfe1c227345b78b870d
Paul Stevenson <paul.mike.stevenson@gmail.com>
2015-12-13 14:25:03 -0600

    Cleaned up Timer script (and increased time to 15s).  Also cleaned up Gameover script and added sound effect to it.

--- LudumDare34/Assets/Scripts/GameOver.js
+++ LudumDare34/Assets/Scripts/GameOver.js
@@ -7,6 +7,11 @@ public var gameover_text : Text;
 public var restart_text : Text;
 public var restart_timer : float = 3.0;
 
+public var soundEffect: AudioClip;
+public var sound_effect_played : boolean = false;
+
+public var bgm_sound : AudioSource;
+
 function GameOver(str : String) {
 	gameover = true;
 	gameover_panel.SetActive(true);
@@ -15,6 +20,11 @@ function GameOver(str : String) {
 
 function Update() {
 	if(gameover) {
+		if(!sound_effect_played) {
+			bgm_sound.volume = 0.2;
+			GetComponent.<AudioSource>().PlayOneShot(soundEffect, 0.5f);
+			sound_effect_played = true;
+		}
 		restart_timer -= Time.deltaTime;
 		if(restart_timer <= 0) {
 			restart_text.color = Color(1,1,1,1);
