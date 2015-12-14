06fb665c49d3c22ef80547aa3f1881b4c9e53227
Paul Stevenson <paul.mike.stevenson@gmail.com>
2015-12-13 22:02:05 -0600

    Fixed timer issue with gameover and gamewin screens.  Also added taunt win condition.

--- LudumDare34/Assets/Scripts/Timer.js
+++ LudumDare34/Assets/Scripts/Timer.js
@@ -10,6 +10,7 @@ public var input_timer_start : float = 3.0;
 public var input_timer : float;
 public var ready_timer : boolean = false;
 public var currently_timing : boolean = false;
+public var game_running : boolean = true;
 public var timer_string : UI.Text;
 public var timer_slider : Slider;
 public var input_string : UI.Text;
@@ -23,31 +24,33 @@ function Awake() {
 
 function Update () {
 
-	if(ready_timer) {
-		if(input_timer >= 0.0) {
-			input_timer -= Time.deltaTime;
-			input_string.text = input_timer.ToString("#.0");
-		}
-		else {
-			input_timer = input_timer_start;
-			input_string.text = "Input Combo!";
-			ready_timer = false;
-			currently_timing = true;
-			input_script.AcceptInput();
-		}
-	}
-	if(currently_timing) {
-		if(timer <= 0.0) {
-			gameover_script.GameOver("Game Over!\nYou ran out of time.  :(");
-		}
-		else {
-			timer -= Time.deltaTime;
-			timer_string.text = timer.ToString("#.0") + "s";
-			if(timer > timer_start) {
-				timer_slider.value = 100;
+	if(game_running) {
+		if(ready_timer) {
+			if(input_timer >= 0.0) {
+				input_timer -= Time.deltaTime;
+				input_string.text = input_timer.ToString("#.0");
 			}
 			else {
-				timer_slider.value = Mathf.Round((timer / timer_start) * 100);
+				input_timer = input_timer_start;
+				input_string.text = "Input Combo!";
+				ready_timer = false;
+				currently_timing = true;
+				input_script.AcceptInput();
+			}
+		}
+		if(currently_timing) {
+			if(timer <= 0.0) {
+				gameover_script.GameOver("Game Over!\nYou ran out of time.  :(");
+			}
+			else {
+				timer -= Time.deltaTime;
+				timer_string.text = timer.ToString("#.0") + "s";
+				if(timer > timer_start) {
+					timer_slider.value = 100;
+				}
+				else {
+					timer_slider.value = Mathf.Round((timer / timer_start) * 100);
+				}
 			}
 		}
 	}
